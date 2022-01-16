7e05da1a51f8ae1cda74589ab664e9bea8fb00e6
jaenny <hongryeon_21@naver.com>
2022-01-16 21:32:39 +0900

    1월 3주차 - 나누어 떨어지는 숫자 배열

---
+++ jaenny/나누어 떨어지는 숫자 배열.py
@@ -0,0 +1,12 @@
+def solution(arr, divisor):
+    answer = []
+    arr = sorted(arr)
+    
+    for num in arr :
+        if num % divisor == 0 :
+            answer.append(num)
+    
+    if len(answer) == 0 :
+        return [-1]
+    
+    return answer
