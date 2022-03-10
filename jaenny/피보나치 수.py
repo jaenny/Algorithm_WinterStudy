1df2f55cc4dd8001bbc4f8a27092796e22c327ae
jaenny <hongryeon_21@naver.com>
2022-03-10 22:28:47 +0900

    3월 3주차 - 피보나치 수

---
+++ jaenny/피보나치 수.py
@@ -0,0 +1,22 @@
+# def solution(n):
+#   answer = 0
+
+#   def Fibo(n) :
+#     if n == 0 : return 0
+#     elif n == 1 : return 1
+#     else :
+#       return Fibo(n-1) + Fibo(n-2)
+  
+#   return Fibo(n)%1234567
+
+def solution(n):
+  answer = 0
+  dp = [0]*100001
+  dp[1] = 1
+  for i in range(2,n+1) :
+    dp[i] = dp[i-1]+dp[i-2]
+  
+  return dp[n]%1234567
+
+
+print(solution(5))
