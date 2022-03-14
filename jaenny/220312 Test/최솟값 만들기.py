7e63762d7f2c6580f81620e3b21193cabc50f51d
jaenny <hongryeon_21@naver.com>
2022-03-14 15:18:54 +0900

    3월 3주차 - 행렬의 곱셈

---
+++ jaenny/220312 Test/최솟값 만들기.py
@@ -0,0 +1,25 @@
+from itertools import permutations
+from math import inf
+def solution(A,B):
+  answer = inf
+
+  multiples = [[0]*len(A) for _ in range(len(A))]
+
+  for i in range(len(A)) :
+    for j in range(len(A)) :
+      multiples[i][j] = A[i]*B[j]
+  
+  # for m in multiples :
+  #   print(m)
+
+  cases = [x for x in permutations(range(len(A)))]
+
+  for case in cases :
+    temp = 0
+    for i in range(len(A)) :
+      temp += multiples[i][case[i]]
+    answer = min(answer, temp)
+
+  return answer
+
+print(solution([1,4,2], [5,4,4]))
