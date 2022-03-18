0cf4be32b3c54b01105cae65b96758f7a05de2cc
jaenny <hongryeon_21@naver.com>
2022-03-18 15:18:03 +0900

    3월 3주차 - 최솟값 만들기

--- jaenny/최솟값 만들기.py
+++ jaenny/최솟값 만들기.py
@@ -1,25 +1,35 @@
-from itertools import permutations
-from math import inf
-def solution(A,B):
-  answer = inf
+# from itertools import permutations
+# from math import inf
+# def solution(A,B):
+#   answer = inf
 
-  multiples = [[0]*len(A) for _ in range(len(A))]
+#   multiples = [[0]*len(A) for _ in range(len(A))]
 
-  for i in range(len(A)) :
-    for j in range(len(A)) :
-      multiples[i][j] = A[i]*B[j]
+#   for i in range(len(A)) :
+#     for j in range(len(A)) :
+#       multiples[i][j] = A[i]*B[j]
   
-  # for m in multiples :
-  #   print(m)
+#   # for m in multiples :
+#   #   print(m)
 
-  cases = [x for x in permutations(range(len(A)))]
+#   cases = [x for x in permutations(range(len(A)))]
 
-  for case in cases :
-    temp = 0
-    for i in range(len(A)) :
-      temp += multiples[i][case[i]]
-    answer = min(answer, temp)
+#   for case in cases :
+#     temp = 0
+#     for i in range(len(A)) :
+#       temp += multiples[i][case[i]]
+#     answer = min(answer, temp)
 
+#   return answer
+
+def solution(A,B) :
+  A.sort()
+  B.sort(reverse=True)
+
+  answer = 0
+  for i in range(len(A)) :
+    answer += A[i]*B[i]
   return answer
+
 
 print(solution([1,4,2], [5,4,4]))
