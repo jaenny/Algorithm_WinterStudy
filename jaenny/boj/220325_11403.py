dc13789c0f493aacc94f17c44cc45cba039956b2
jaenny <hongryeon_21@naver.com>
2022-03-25 13:40:47 +0900

    220325 11403 - 경로 찾기

---
+++ jaenny/boj/220325_11403.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+matrix = [list(map(int,input().split())) for _ in range(n)]
+visit = [[0]*n for _ in range(n)]
+for k in range(n) : # 경유지
+  for i in range(n) : # 출발지
+    for j in range(n) : # 도착
+      if matrix[i][j] == 1 : continue
+      elif (matrix[i][k] == 1 and matrix[k][j] == 1) :
+        matrix[i][j] = 1
+      
+
+for m in matrix :
+  print(' '.join(map(str,m)))
